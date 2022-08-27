250e11bdb0de8ab7aeb577bc51f1c4178342e175
tanhocnn - Win1909.WORK <tanhocnn@gmail.com>
2022-08-26 22:52:14 -0700

    Bai 3.2

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace BookMan.ConsoleApp.Models
 {
-    class Boook
+    public class Book
     {
-        int  _id;
-        string _authors;
-        string _title;
-        string _publisher;
-        int _year;
-        int _edition;
-        string _isbn;
-        string _tags;
-        string _description;
-        int _rating;
-        bool _reading;
-        string _file;
+        public int Id = 1;
+        public string Authors = "Unknown authors";
+        public string Title = "A new book";
+        public string Publisher = "Unknown publisher";
+        public int Year = 2018;
+        public int Edition = 1;
+        public string Isbn;
+        public string Tags;
+        public string Description = "A new book";
+        public int Rating = 1;
+        public bool Reading = false;
+        public string File;
     }
 }
