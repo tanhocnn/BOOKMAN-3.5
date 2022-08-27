4efe7f4feab6104d6086d1d155f5f69c868c1995
tanhocnn - Win1909.WORK <tanhocnn@gmail.com>
2022-08-26 22:44:13 -0700

    bai 3.1

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMan.ConsoleApp.Models
+{
+    class Boook
+    {
+        int  _id;
+        string _authors;
+        string _title;
+        string _publisher;
+        int _year;
+        int _edition;
+        string _isbn;
+        string _tags;
+        string _description;
+        int _rating;
+        bool _reading;
+        string _file;
+    }
+}
