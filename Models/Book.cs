e0124bff73516ce6b9d4f109efcf16c1a6d40c1a
tanhocnn - Win1909.WORK <tanhocnn@gmail.com>
2022-08-27 19:23:33 -0700

    Bai 3.5

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,15 +6,25 @@ using System.Threading.Tasks;
 
 namespace BookMan.ConsoleApp.Models
 {
+    /// <summary>
+    /// Lop mo ta sach dien tu
+    /// </summary>
     public class Book
     {
         private int _id = 1;
+        /// <summary>
+        /// so dinh danh duy nhat cho moi object
+        /// </summary>
         public int Id
         {
             get { return _id; }
             set { if (value >= 1) _id = value; }
         }
         private string _authors;
+        /// <summary>
+        /// ten tac gia hoac nhom tac gia
+        /// </summary>
+        /// <value></value>
         public string Authors
         {
             get { return _authors; }
@@ -22,6 +32,10 @@ namespace BookMan.ConsoleApp.Models
         }
 
         private string _title = "A new book";
+        /// <summary>
+        /// tieu de sach, khong nhan sau rong
+        /// </summary>
+        /// <value></value>
         public string Title
         {
             get { return _title; }
@@ -29,6 +43,10 @@ namespace BookMan.ConsoleApp.Models
         }
         
         private string _publisher = "Unknown publisher";
+        /// <summary>
+        /// nha xuat ban, khong nhan xau rong
+        /// </summary>
+        /// <value></value>
         public string Publisher
         {
             get { return _publisher; }
@@ -37,6 +55,10 @@ namespace BookMan.ConsoleApp.Models
         }
 
         private int _year = 2018;
+        /// <summary>
+        /// nam xuat ban, khong nho hon 1950
+        /// </summary>
+        /// <value></value>
         public int Year
         {
             get { return _year; }
@@ -45,6 +67,10 @@ namespace BookMan.ConsoleApp.Models
         }
 
         private int _edition = 1;
+        /// <summary>
+        /// lan tai ban, khong nho hon 1
+        /// </summary>
+        /// <value></value>
         public int Edition
         {
             get { return _edition; }
@@ -52,23 +78,51 @@ namespace BookMan.ConsoleApp.Models
 
         }
 
+        /// <summary>
+        /// ma so quoc te
+        /// </summary>
+        /// <value></value>
         public string Isbn {get; set; } = "";
+        /// <summary>
+        /// tu khoa mo ta noi dung / the loai
+        /// </summary>
+        /// <value></value>
         public string Tags { get; set; } = "";
+        /// <summary>
+        /// mo ta tom tat noi dung
+        /// </summary>
+        /// <value></value>
         public string Description { get; set; } = "A new book";
         private int _rating = 1;
+        /// <summary>
+        /// danh gia ca nhan, gia tri tu 1 den 5
+        /// </summary>
+        /// <value></value>
         public int Rating 
         {
             get { return _rating; }
             set { if (value>=1 && value<=5) _rating = value; }
         }
 
+        /// <summary>
+        /// danh dau la dang doc
+        /// </summary>
+        /// <value></value>
         public bool Reading { get; set; }
         private string _file;
+        /// <summary>
+        /// file sach gom duong dan
+        /// </summary>
+        /// <value></value>
         public string File
         {
             get { return _file; }
             set { if (System.IO.File.Exists(value)) _file = value; }
         }
+        /// <summary>
+        /// file sach khong co duong dan
+        /// </summary>
+        /// <value></value>
         public string FileName
         {
             get { return System.IO.Path.GetFileName(_file); }
